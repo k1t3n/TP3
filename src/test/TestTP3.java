570abffebf36888197eba9146a60b248bb7216ba
KittenRoar <yanickvali@gmail.com>
2019-03-20 11:04:20 -0400

    Ajout du fichier incluant les tests

---
+++ src/test/TestTP3.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import main.Client;
+import main.Commande;
+import main.Facture;
+import main.Plat;
+
+public class TestTP3 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPlatExiste() {
+		fail( "Not yet implemented" );
+	}
+	
+	@Test
+	public void testNomPresent() {
+		
+		Client clientTemp = new Client("Jean");
+		
+		
+	}
+	
+	@Test
+	public void testChiffresErrones() {		
+		
+	}
+	
+	@Test
+	public void testSortiFacture() {
+		
+	}
+
+}
